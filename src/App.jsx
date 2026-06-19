f60546dc084e8e657478f2a0b5021e3836746dbd
merajul-islam644 <merajul.islam@selisegroup.com>
2026-06-19 22:33:06 +0600

    refactor: remove unused components and authentication hooks; simplify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import React from "react";
-import Draggable from "react-draggable";
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#f0f0f0",
-      }}
-    >
-      <Draggable>
-        <div
-          style={{
-            width: 200,
-            padding: 20,
-            background: "#3498db",
-            color: "white",
-            cursor: "move",
-            borderRadius: 10,
-            textAlign: "center",
-            fontSize: 20,
-            userSelect: "none",
-          }}
-        >
-          Drag Me
-        </div>
-      </Draggable>
+    <div>
+      <h1>This is App.</h1>
     </div>
-  );
+  )
 }
+
+export default App;
